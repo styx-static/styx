4b5a6eaadcb7351068a33d4e742ab1e75337b794
Eric Sagnes <eric.sagnes@gmail.com>
2016-10-13 09:32:20 +0900

    css preprocessors support (sass/less)

--- roadmap.md
+++ roadmap.md
@@ -2,15 +2,17 @@
 
 ## 0.3.0
 
-- [ ] port some Hugo themes to Styx ([agency](http://themes.gohugo.io/agency/) and [creative](http://themes.gohugo.io/creative/) are simple and good eye candy)
-- [ ] Saas / less css preprocessing
+- [x] port othe static gen themes to Styx ([agency](http://themes.gohugo.io/agency/) and [hyde](https://github.com/poole/hyde)
+- [x] taxonomies
+- [x] Add a way to upgrade versions (lib and themes)
+- [x] Saas / less css preprocessing
+- [ ] polishing default theme (something like https://github.com/appernetic/hugo-bootstrap-premium could be nice)
+- [ ] make sample content act like a lightweight documentation (How drafts woks, quickstart, themes)
+- [ ] Update documentation...
 
 
-## Post 0.3.0
+## Features to implement
 
-- [ ] taxonomies
-- [ ] Improve default theme design (something like https://github.com/appernetic/hugo-bootstrap-premium could be nice)
-- [ ] Add a way to upgrade versions
 - [ ] Linkcheck functionality
 - [ ] Syntax highlighting
 - [ ] Automatic summary (Taking the X first words of a post)
--- src/lib/generation.nix
+++ src/lib/generation.nix
@@ -21,23 +21,47 @@ rec {
   generateSite = {
     files
   , pagesList
-  , preGen ? ""
+  , preGen  ? ""
   , postGen ? ""
+  , enablePreprocessors ? false
   }:
     pkgs.runCommand "styx-site" {} ''
+      shopt -s globstar
       mkdir -p $out
 
-      ${concatMapStringsSep "\n" (filesDir: ''
-      (
-        cd ${filesDir}
-        for file in ./*; do
-          if [ ! -e "$out/$file" ]; then
-            ln -s "$(pwd)/$file" "$out/$file"
-          fi
-        done
-      )
-      '') files}
+      eval "${preGen}"
 
+      # Copying files
+      ${concatMapStringsSep "\n" (filesDir:
+        if enablePreprocessors then ''
+
+          cp -r ${filesDir}/* $out
+          chmod u+rw -R $out/
+
+          # convert sass / scss
+          for i in $out/**/*.s[ac]ss; do
+            ${pkgs.sass}/bin/sass $i > "$(realpath $i | cut -d'.' -f1).css"
+            rm $i
+          done
+
+          # convert less
+          for i in $out/**/*.less; do
+            ${pkgs.lessc}/bin/lessc $i > "$(realpath $i | cut -d'.' -f1).css"
+            rm $i
+          done
+
+        '' else ''
+        (
+          cd ${filesDir}
+          for file in ./*; do
+            if [ ! -e "$out/$file" ]; then
+              ln -s "$(pwd)/$file" "$out/$file"
+            fi
+          done
+        )
+        '') files}
+
+      # Generating pages
       ${concatMapStringsSep "\n" (page: ''
         mkdir -p $(dirname $out/${page.href})
         ln -s ${pkgs.writeText "styx-site-${replaceStrings ["/"] ["-"] page.href}" (page.layout (page.template page)) } $out/${page.href}
