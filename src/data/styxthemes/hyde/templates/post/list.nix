81399676586b0bd0449200939f6e4d953b52bb59
Eric Sagnes <eric.sagnes@gmail.com>
2016-10-19 10:09:00 +0900

    styx 0.3.0

--- src/data/styxthemes/hyde/templates/post/list.nix
+++ src/data/styxthemes/hyde/templates/post/list.nix
@@ -5,6 +5,6 @@ page:
   <div class="post">
     <h1><a href="${conf.siteUrl}/${page.href}">${page.title}</a></a></h1>
     <span class="post-date">${with (parseDate page.date); "${D} ${b} ${Y}"}</span>
-    ${page.content}
+    ${page.intro}
   </div>
 ''
