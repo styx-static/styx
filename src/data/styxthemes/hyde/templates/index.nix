7e9434024a5280162b237e0f1fe2db0bc09277a3
danbst <abcz2.uprola@gmail.com>
2016-10-29 05:15:58 +0000

    add some defaults, so we don't have to specify all theme attributes in site.nix

--- src/data/styxthemes/hyde/templates/index.nix
+++ src/data/styxthemes/hyde/templates/index.nix
@@ -4,10 +4,10 @@ with lib;
 let content =
   ''
   <div class="posts">
-    ${mapTemplate templates.post.list page.items}
+    ${mapTemplate templates.post.list (page.items or [])}
   </div>
 
-  ${templates.partials.pagination { pages = page.pages; index = page.index; }} 
+  ${templates.partials.pagination { pages = (page.pages or []); index = page.index; }} 
   '';
 in
  page // { inherit content; }
