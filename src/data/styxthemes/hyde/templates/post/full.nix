891515fcd44ca8e3115ec018c82b7d8ab1882dac
Eric Sagnes <eric.sagnes@gmail.com>
2016-12-17 13:44:53 +0900

    prepare to styx 0.5.0

--- src/data/styxthemes/hyde/templates/post/full.nix
+++ src/data/styxthemes/hyde/templates/post/full.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, templates, ... }:
 page:
 with lib;
 let content =
@@ -7,6 +7,8 @@ let content =
       <h1>${page.title}</h1>
       <span class="post-date">${with (parseDate page.date); "${D} ${b} ${Y}"}</span>
       ${page.content}
+
+      ${optionalString (page ? pages) (templates.partials.pagination { pages = page.pages; index = page.index; })}
     </div>
   '';
 in
