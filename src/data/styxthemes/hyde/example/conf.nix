22066ec861d7f0000aecd5df53ea10847f38aa92
Eric Sagnes <eric.sagnes@gmail.com>
2017-01-17 17:54:49 +0900

    configuration interface

--- src/data/styxthemes/hyde/example/conf.nix
+++ src/data/styxthemes/hyde/example/conf.nix
@@ -2,10 +2,7 @@
   # URL of the site, must be set to the url of the domain the site will be deployed
   siteUrl = "https://styx-static.github.io/styx-theme-hyde";
 
-  # Theme specific settings
-  # it is possible to override any of the theme configuration settings in the 'theme' set
   theme = {
-    # Example, changing the default theme site.title setting
     site.title = "Styx Site";
   };
 }
