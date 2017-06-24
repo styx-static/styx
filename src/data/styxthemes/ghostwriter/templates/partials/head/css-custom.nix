e08e7acae4a562f79250324ec95a04c4d883149c
Eric Sagnes <eric.sagnes@gmail.com>
2017-06-24 12:59:12 +0900

    typos fix

--- src/data/styxthemes/ghostwriter/templates/partials/head/css-custom.nix
+++ src/data/styxthemes/ghostwriter/templates/partials/head/css-custom.nix
@@ -1,4 +1,3 @@
 { templates, ... }:
 page:
   templates.tag.link-css { href = templates.url "/styles/style.css"; }
-+ templates.tag.link-css { href = "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin,cyrillic-ext,latin-ext,cyrillic"; }
