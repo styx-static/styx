51762d7d5e2bcc8ce4e794e3ac5e824fa0de4f31
Eric Sagnes <eric.sagnes@gmail.com>
2017-08-06 20:30:44 +0900

    derivation file

---
+++ src/data/styxthemes/showcase/derivation.nix
@@ -0,0 +1,9 @@
+{ stdenv }:
+let
+  theme = "showcase";
+  version = "dev";
+in stdenv.mkDerivation rec {
+  name    = "${theme}-${version}";
+  src     =  ./.;
+  installPhase = ''mkdir $out && cp -r $src/* $out/'';
+}
