c1721db5e52ce3065fb630c0a358edfbea04b8a0
Eric Sagnes <eric.sagnes@gmail.com>
2017-08-11 13:03:36 +0900

    remove unused lang template

--- src/data/styxthemes/orbit/templates/partials/lang.nix
+++
@@ -1,6 +0,0 @@
-{ conf, ... }:
-''
-<!--[if IE 8]> <html lang="{{ with .Site.LanguageCode }}{{ . }}{{ else }}en-US{{ end }}" class="ie8"> <![endif]-->  
-<!--[if IE 9]> <html lang="{{ with .Site.LanguageCode }}{{ . }}{{ else }}en-US{{ end }}" class="ie9"> <![endif]-->  
-<!--[if !IE]><!--> <html lang="{{ with .Site.LanguageCode }}{{ . }}{{ else }}en-US{{ end }}"> <!--<![endif]-->
-''
