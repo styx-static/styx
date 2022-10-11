234a62389163ba72c12b1450b9c25c83e7b418f1
David Arnold <david.arnold@iohk.io>
2022-10-10 20:37:11 -0500

    style: treefmt css, scss, json & js

--- src/data/styxthemes/agency/files/js/agency.js
+++ src/data/styxthemes/agency/files/js/agency.js
@@ -5,36 +5,43 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-  /* Fixing full urls for scrollspy
-  */
-  var url = location.href.replace(/#.*/,'');
-  $(".navbar-fixed-top").find('a[href]').each(function(i,a) {
-    var $a = $(a);
-    var href = $a.attr('href');
-    if (href.indexOf(url+'#') == 0) {
-      href = href.replace(url,'');
-      $a.attr('href',href);
-    }
-    $('body').scrollspy('refresh')
+$(function () {
+  $("a.page-scroll").bind("click", function (event) {
+    var $anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top,
+        },
+        1500,
+        "easeInOutExpo"
+      );
+    event.preventDefault();
   });
 
+  /* Fixing full urls for scrollspy
+   */
+  var url = location.href.replace(/#.*/, "");
+  $(".navbar-fixed-top")
+    .find("a[href]")
+    .each(function (i, a) {
+      var $a = $(a);
+      var href = $a.attr("href");
+      if (href.indexOf(url + "#") == 0) {
+        href = href.replace(url, "");
+        $a.attr("href", href);
+      }
+      $("body").scrollspy("refresh");
+    });
 });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+$("body").scrollspy({
+  target: ".navbar-fixed-top",
+});
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+$(".navbar-collapse ul li a").click(function () {
+  $(".navbar-toggle:visible").click();
 });
